5f540bbe95a900e034a99f92fc5670d080ae777c
SmoxBoye <simon.sbg@gmail.com>
2025-12-15 13:31:21 +0100

    restructure

--- home.nix
+++ home.nix
@@ -6,23 +6,15 @@
   home.homeDirectory = "/home/smoxboye";
   home.stateVersion = "25.11";
 
-  # User packages
-  home.packages = with pkgs; [
-    kdePackages.kate
-    vesktop
-    git
-    helix
-    playerctl
-    ghostty
-    lazygit
-  ];
-
   # Enable programs
-  programs.yazi.enable = true;
   programs.home-manager.enable = true;
 
   # Import Home Manager modules
   imports = [
     ./modules/home-manager/browser.nix
+    ./modules/home-manager/development.nix
+    ./modules/home-manager/terminal.nix
+    ./modules/home-manager/desktop.nix
+    ./modules/home-manager/communication.nix
   ];
 }
