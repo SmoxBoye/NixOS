e3977ff882982334c071454bda5b8d1150f5c2fa
SmoxBoye <simon.sbg@gmail.com>
2025-12-18 21:47:42 +0100

    FORMAT ALL THE CONFIG :D

--- modules/nixos/development.nix
+++ modules/nixos/development.nix
@@ -1,4 +1,4 @@
 { config, pkgs, ... }:
 {
-    programs.nix-ld.enable = true;
+  programs.nix-ld.enable = true;
 }
--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,49 +1,55 @@
 { config, pkgs, ... }:
 {
-    programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-        extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
 
-        gamescopeSession.enable = true;
+    gamescopeSession.enable = true;
 
-        package = pkgs.steam.override {
-            extraPkgs = pkgs:
-            with pkgs; [
-                xorg.libXcursor
-                xorg.libXi
-                xorg.libXinerama
-                xorg.libXScrnSaver
-                libpng
-                libpulseaudio
-                libvorbis
-                stdenv.cc.cc.lib
-                libkrb5
-                keyutils
-                wayland
-                libxkbcommon
-                vulkan-loader
-                vulkan-validation-layers
-                gamescope
-            ];
-        };
-
-      extraPackages = with pkgs; [ mangohud ];
+    package = pkgs.steam.override {
+      extraPkgs =
+        pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+          wayland
+          libxkbcommon
+          vulkan-loader
+          vulkan-validation-layers
+          gamescope
+        ];
     };
 
-    programs.gamemode = {
-      enable = true;
-      enableRenice = true;
-      settings = { general = { renice = 10; }; };
+    extraPackages = with pkgs; [ mangohud ];
+  };
+
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+    settings = {
+      general = {
+        renice = 10;
+      };
     };
+  };
 
-    programs.gamescope = {
-      enable = true;
-      capSysNice = false;
-    };
+  programs.gamescope = {
+    enable = true;
+    capSysNice = false;
+  };
 
-    environment.systemPackages = with pkgs; [ mangohud gamemode ];
-
+  environment.systemPackages = with pkgs; [
+    mangohud
+    gamemode
+  ];
 
 }
