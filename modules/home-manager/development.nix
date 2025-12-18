b08cee4f4f2aa23740cffba9ed747a8b44700a70
SmoxBoye <simon.sbg@gmail.com>
2025-12-18 21:47:28 +0100

    add nix lsp and formatting

--- modules/home-manager/development.nix
+++ modules/home-manager/development.nix
@@ -5,10 +5,12 @@
     git
     lazygit
     uv
+    nil
+    nixfmt-rfc-style
   ];
 
   # Clone dotfiles repo to home directory
-  home.activation.cloneHelixDotfiles = config.lib.dag.entryAfter ["writeBoundary"] ''
+  home.activation.cloneHelixDotfiles = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "$HOME/.config/helix" ]; then
       ${pkgs.git}/bin/git clone https://github.com/SmoxBoye/helix-config.git "$HOME/.config/helix"
     fi
