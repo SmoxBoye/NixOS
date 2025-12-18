0ff7173ec164b4301a7a8f5529142a37226a6242
SmoxBoye <simon.sbg@gmail.com>
2025-12-18 21:47:42 +0100

    FORMAT ALL THE CONFIG :D

--- flake.nix
+++ flake.nix
@@ -5,22 +5,29 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-};
-
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
-    nixosConfigurations.SmoxPC = nixpkgs.lib.nixosSystem {
-      specialArgs = { 
-        inherit inputs;
-      };
-      modules = [
-        ./hosts/SmoxPC/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.smoxboye = import ./home.nix;
-        }
-      ];
-    };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.SmoxPC = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./hosts/SmoxPC/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.smoxboye = import ./home.nix;
+          }
+        ];
+      };
+    };
 }
--- hosts/SmoxPC/configuration.nix
+++ hosts/SmoxPC/configuration.nix
@@ -5,17 +5,16 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../modules/nixos/core/boot.nix
-      ../../modules/nixos/core/network.nix
-      ../../modules/nixos/core/locale.nix
-      ../../modules/nixos/graphics.nix
-      ../../modules/nixos/gaming.nix
-      ../../modules/nixos/development.nix
-    ];
-
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/nixos/core/boot.nix
+    ../../modules/nixos/core/network.nix
+    ../../modules/nixos/core/locale.nix
+    ../../modules/nixos/graphics.nix
+    ../../modules/nixos/gaming.nix
+    ../../modules/nixos/development.nix
+  ];
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
@@ -51,22 +50,25 @@
   users.users.smoxboye = {
     isNormalUser = true;
     description = "Simon Bengtsson";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
   };
-
-
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
