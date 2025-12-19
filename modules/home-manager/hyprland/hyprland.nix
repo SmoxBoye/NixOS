859c020d2d86e16eeb9df7f1c0e8670ee86e102d
SmoxBoye <simon.sbg@gmail.com>
2025-12-19 01:02:06 +0100

    wayland hint should be home managed aswell

--- modules/home-manager/hyprland/hyprland.nix
+++ modules/home-manager/hyprland/hyprland.nix
@@ -1,7 +1,7 @@
-{ config, pkgs }:
+{ config, pkgs, ... }:
 {
   programs.kitty.enable = true; # required for the default Hyprland config
   wayland.windowManager.hyprland.enable = true; # enable Hyprland
   # Optional, hint Electron apps to use Wayland:
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
