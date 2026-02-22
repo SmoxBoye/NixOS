df8b543a5f77af5d663307b523ff6650f8d20b10
SmoxBoye <simon.sbg@gmail.com>
2026-02-22 23:57:42 +0100

    More steam

---
+++ modules/nixos/steam.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+let
+  patchedBwrap = pkgs.bubblewrap.overrideAttrs (o: {
+    patches = (o.patches or [ ]) ++ [
+      ./bwrap.patch
+    ];
+  });
+in
+{
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override {
+      buildFHSEnv = (
+        args:
+        (
+          (pkgs.buildFHSEnv.override {
+            bubblewrap = patchedBwrap;
+          })
+          (
+            args
+            // {
+              extraBwrapArgs = (args.extraBwrapArgs or [ ]) ++ [ "--cap-add ALL" ];
+            }
+          )
+        )
+      );
+      extraPkgs =
+        pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+          wayland
+          libxkbcommon
+          vulkan-loader
+          vulkan-validation-layers
+          gamescope
+        ];
+
+    };
+  };
+}
